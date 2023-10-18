a39415ac81ce5f816711c1804b3182c4a4e6b3e7
rpraveenkumar1220 <rpraveenkumar1220@gmail.com>
2023-10-18 09:33:13 +0530

    Roboshop with terraform setup

--- main.tf
+++ main.tf
@@ -1,7 +1,6 @@
 resource "aws_vpc" "main" {
   cidr_block          = var.cidr_block
   enable_dns_support  = true
-
   tags = {
     Name = "${var.env}-vpc"
   }
@@ -21,7 +20,6 @@ resource "aws_vpc_peering_connection" "peer" {
   peer_vpc_id   = aws_vpc.main.id
   vpc_id        = var.default_vpc_id
   auto_accept   = true
-
   tags = {
     Name = "defaul-vpc to ${var.env}-vpc "
   }
@@ -29,7 +27,6 @@ resource "aws_vpc_peering_connection" "peer" {
 
 resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.main.id
-
   tags = {
     Name = "${var.env}-vpc-igw"
   }
@@ -47,16 +44,28 @@ resource "aws_eip" "el-Nat" {
 }
 
 
-resource "aws_nat_gateway" "example" {
+resource "aws_nat_gateway" "ngw" {
   allocation_id = aws_eip.el-Nat.id
   subnet_id     = lookup(lookup(module.subnets, "public", null), "subnet_ids" , null)[0]
-
   tags = {
     Name = "${var.env}-public-NAT"
   }
-
-
-  depends_on = [aws_internet_gateway.igw]
 }
+
+resource "aws_route" "ngw_route" {
+  count = length(local.private_route_table_ids)
+  route_table_id            = element(local.private_route_table_ids,count.index)
+  destination_cidr_block    = "0.0.0.0/16"
+  gateway_id = aws_nat_gateway.ngw.id
+}
+
+resource "aws_route" "peer_route" {
+  count = length(local.all_route_table_ids)
+  route_table_id            = element(local.all_route_table_ids,count.index)
+  destination_cidr_block    = "172.31.0.0/16"
+  vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
+}
+
+
 
 
--- subnets/main.tf
+++ subnets/main.tf
@@ -2,7 +2,6 @@ resource "aws_subnet" "subnet" {
   count = length(var.cidr_block)
   vpc_id     = var.vpc_id
   cidr_block = element(var.cidr_block,count.index)
-
   tags = {
     Name = "${var.env}-${var.subnet_name}-subnet"
   }
@@ -13,9 +12,13 @@ resource "aws_subnet" "subnet" {
 
 resource "aws_route_table" "route_table" {
   vpc_id = var.vpc_id
-
-
   tags = {
     Name = "${var.env}-${var.subnet_name}-rt"
   }
+}
+
+resource "aws_route_table_association" "association" {
+  count = length(aws_subnet.subnet.*.id)
+  subnet_id      = element(aws_subnet.subnet.*.id, count.index)
+  route_table_id = aws_route_table.route_table.id
 }
