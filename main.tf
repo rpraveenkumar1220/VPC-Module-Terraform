a3d7bcf64890b4002078de1f9a0c1003bbffd554
rpraveenkumar1220 <rpraveenkumar1220@gmail.com>
2023-11-01 21:32:29 +0530

    Roboshop with terraform setup

--- main.tf
+++ main.tf
@@ -66,6 +66,11 @@ resource "aws_route" "peer_route" {
   vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
 }
 
+resource "aws_route" "default_vpc_peer_route" {
+  route_table_id            = var.default_vpc_rt
+  destination_cidr_block    = var.cidr_block
+  vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
+}
 
 
 
--- variables.tf
+++ variables.tf
@@ -2,3 +2,4 @@ variable "env" {}
 variable "cidr_block" {}
 variable "subnets" {}
 variable "default_vpc_id" {}
+variable "default_vpc_rt" {}
